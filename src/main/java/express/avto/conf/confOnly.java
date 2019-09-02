b25ead579bfbaadc55baab82f7c5004bd6d77b9c
Vladimirov Stanislav <zstanislaw211@gmail.com>
2019-09-02 10:17:31 +0300

    first commit

---
+++ src/main/java/express/avto/conf/confOnly.java
@@ -0,0 +1,27 @@
+package express.avto.conf;
+
+public class confOnly {
+	/*
+	#Git
+	 
+	 git pull
+	 
+	 
+	 cd C:/Users/svladimirov/eclipse-workspace/express.avto
+	  
+	 git add . 
+	 	 
+	 git commit -m  "first commit" 
+	 git push -u origin master
+	 
+	 
+	 #maven
+	 cd C:/Users/svladimirov/eclipse-workspace/express.avto
+	 mvn clean compile assembly:single
+
+	 #Run myProg
+	 #java -jar C:\vianor_stock\ru.badyavina.www-0.0.1-SNAPSHOT-jar-with-dependencies.jar "C:\vianor_stock" "C:\vianor_stock" "Products_Vianor_589_32202gg.csv" "C:\vianor_stock\CentralProvider" "C:\vianor_stock\OtherProvider" "Stanislav.Vladimirov@nokiantyres.com" "zstanislaw211@gmail.com"
+	 
+	 */
+
+}
